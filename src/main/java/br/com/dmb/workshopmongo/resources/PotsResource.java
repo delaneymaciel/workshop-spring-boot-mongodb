02b4e542f82acad3cb0dc1f53ffd40c480a7786f
Delaney Maciel <delaneymaciel@hotmail.com>
2021-07-22 22:44:30 -0300

    obtendo post por id

---
+++ src/main/java/br/com/dmb/workshopmongo/resources/PotsResource.java
@@ -0,0 +1,29 @@
+package br.com.dmb.workshopmongo.resources;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.dmb.workshopmongo.domain.Post;
+import br.com.dmb.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PotsResource {
+	
+	@Autowired
+	private PostService service;
+	
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post obj = service.findById(id);
+		return  ResponseEntity.ok().body(obj);
+	}	
+
+	
+}
---
+++ src/main/java/br/com/dmb/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package br.com.dmb.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.dmb.workshopmongo.domain.Post;
+import br.com.dmb.workshopmongo.domain.User;
+import br.com.dmb.workshopmongo.repository.PostRepository;
+import br.com.dmb.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado.")); 
+	}
+	
+
+
+}
